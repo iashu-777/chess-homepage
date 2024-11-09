026c608d4655aad0ead11680301b1571456099e8
iashu-777 <2k23.csds2310610@gmail.com>
2024-11-09 10:55:55 +0530

    hey

--- socket.js
+++ socket.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 
 // Configure CORS with middleware
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: "http://localhost:3000",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
 };
