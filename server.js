48baa6a31a758bcfa496363b6a5592155389c025
iashu-777 <2k23.csds2310610@gmail.com>
2024-11-25 00:06:27 +0530

    hey

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 // MongoDB connection
 mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect('mongodb+srv://iashu-777:fatherhadadonkey@cluster0.xm1o9.mongodb.net/chess_db?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => {
       console.error('Error connecting to MongoDB:', err);
